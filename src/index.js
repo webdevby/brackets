f5f8d0a796a4093dbeda4ee89ba13f47b9848f5f
webdevby <138303565+webdevby@users.noreply.github.com>
2024-10-27 22:06:19 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  function check(str, bracketsConfig) {
+	console.log(bracketsConfig.flat());
+	let arr = [];
+	let openBrackets = [];
+	let closeBrackets = [];
+	
+	
+	bracketsConfig.forEach((el) => {
+		openBrackets.push(el[0]);
+		closeBrackets.push(el[1]);
+	});
+
+	for(let i = 0; i < str.length; i++) {
+		
+		if(str.length % 2 !== 0) return false;
+		if(bracketsConfig.flat().indexOf(str[i]) !== -1) return false;
+
+		//check for special case
+		if(closeBrackets.indexOf(str[i] !== -1) && openBrackets.indexOf(str[i]) !== -1) {
+			if(str[i] === arr.at(-1)) arr.pop();
+			else arr.push(str[i]);
+			continue;
+		}
+
+		if(openBrackets.indexOf(str[i]) !== -1) {
+			arr.push(str[i]);
+		}
+		else if(closeBrackets.indexOf(str[i]) !== -1) {
+			if(closeBrackets.indexOf(str[i]) === openBrackets.indexOf(arr.at(-1))) arr.pop();
+		}
+	}
+
+	return arr.length === 0;
+}
+
+
+
+
 }
